a1b7326e0d4b076da6f439f3787df0b064d4773e
rjduffner <rjduffner@gmail.com>
2013-07-03 00:44:57 -0700

    added global variables instead of hardcode

--- download_all_repos.sh
+++ download_all_repos.sh
@@ -3,17 +3,21 @@
 #
 # Edited for site by Robert Duffner
 # Credit to kenorb of Drupal forms for base code
-# Adding Git Comment
 #
+# curl https://github.com/rjduffner?tab=repositories | perl -nle 'print "$2" if /(<a href=.*rjduffner\/(.*)">.*<\/a>)/' >project_list.txt
 
-curl https://github.com/rjduffner?tab=repositories | perl -nle 'print "$2" if /(<a href=.*rjduffner\/(.*)">.*<\/a>)/' >project_list.txt
+GITURL="https://github.com/"
+GITUSER="rjduffner"
+
+curl $GITURL$GITUSER?tab=repositories | perl -nle 'print "$2" if /(<a href=.*'$GITUSER'\/(.*)">.*<\/a>)/' >project_list.txt
+for project in $(cat project_list.txt)
 do
   echo $project
   if [ ! -d $project/.git ]; then
     if [ -d $project ]; then
       rm -rf $project
     fi
-    git clone https://github.com/rjduffner/$project.git
+    git clone $GITURL$GITUSER/$project.git
   else
     git --git-dir=$project/.git --work-tree=$project fetch
   fi
